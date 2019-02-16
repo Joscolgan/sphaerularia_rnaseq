355c8bf56fcc0d56f595fea8376df02c46001536
Thomas Colgan <btw928@frontend5.apocrita>
2019-02-16 17:41:02 +0000

    Add script for renaming files

---
+++ 01_quality_assessment/rename_file.sh
@@ -0,0 +1,44 @@
+#!/bin/env bash
+## Diapause samples:
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-001_1.solfastq.gz diapause_infected_1.R1.fastq.gz
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-001_2.solfastq.gz diapause_infected_1.R2.fastq.gz
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-002_1.solfastq.gz diapause_infected_2.R1.fastq.gz
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-002_2.solfastq.gz diapause_infected_2.R2.fastq.gz 
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-003_1.solfastq.gz diapause_infected_3.R1.fastq.gz 
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-003_2.solfastq.gz diapause_infected_3.R2.fastq.gz 
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-004_1.solfastq.gz diapause_control_1.R1.fastq.gz 
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-004_2.solfastq.gz diapause_control_1.R2.fastq.gz 
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-005_1.solfastq.gz diapause_control_2.R1.fastq.gz 
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-005_2.solfastq.gz diapause_control_2.R2.fastq.gz 
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-006_1.solfastq.gz diapause_control_3.R1.fastq.gz 
+ln -s ../tmp_raw_sequences/110418_0015_707VLAAXX_7_SA-PE-006_2.solfastq.gz diapause_control_3.R2.fastq.gz
+
+## Postdiapause samples:
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-031_1.sanfastq.gz postdiapause_infected_1.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-031_2.sanfastq.gz postdiapause_infected_1.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-032_1.sanfastq.gz postdiapause_infected_2.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-032_2.sanfastq.gz postdiapause_infected_2.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-033_1.sanfastq.gz postdiapause_infected_3.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-033_2.sanfastq.gz postdiapause_infected_3.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-034_1.sanfastq.gz postdiapause_infected_4.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-034_2.sanfastq.gz postdiapause_infected_4.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-035_1.sanfastq.gz postdiapause_infected_5.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-035_2.sanfastq.gz postdiapause_infected_5.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-036_1.sanfastq.gz postdiapause_infected_6.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-036_2.sanfastq.gz postdiapause_infected_6.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-037_1.sanfastq.gz postdiapause_infected_7.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-037_2.sanfastq.gz postdiapause_infected_7.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-038_1.sanfastq.gz postdiapause_control_1.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-038_2.sanfastq.gz postdiapause_control_1.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-039_1.sanfastq.gz postdiapause_control_2.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-039_2.sanfastq.gz postdiapause_control_2.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-040_1.sanfastq.gz postdiapause_control_3.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-040_2.sanfastq.gz postdiapause_control_3.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-041_1.sanfastq.gz postdiapause_control_4.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-041_2.sanfastq.gz postdiapause_control_4.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-042_1.sanfastq.gz postdiapause_control_5.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-042_2.sanfastq.gz postdiapause_control_5.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-043_1.sanfastq.gz postdiapause_control_6.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-043_2.sanfastq.gz postdiapause_control_6.R2.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-044_1.sanfastq.gz postdiapause_control_7.R1.fastq.gz
+ln -s ../tmp_raw_sequences/120604_0194_AC0PTKACXX_7_SA-PE-044_2.sanfastq.gz postdiapause_control_7.R2.fastq.gz
