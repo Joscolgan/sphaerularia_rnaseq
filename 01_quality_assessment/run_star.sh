8e0dfb611a441fd2b579cf30be7aaa15bc086450
Joe Colgan <t.colgan@qmul.ac.uk>
2019-04-21 22:28:40 +0100

    Included header information

--- 01_quality_assessment/run_star.sh
+++ 01_quality_assessment/run_star.sh
@@ -1,10 +1,26 @@
-for name in ./input/*_1*fastq;
+#!/bin/env bash
+########################################################################
+##
+## Author: Joe Colgan                     Name: run_star.sh
+##
+## Purpose:
+## This script takes pairs of compressed fastq files and performs 
+## alignment against a database of indexed transcripts using the 
+## RNA-Seq aligner, STAR. The output sam file is converted in bam file
+## format and sorted by read coordinates.
+## The final output is a bam file
+##
+########################################################################
+
+for name in ./input/*_1*fastq.gz;
 do
 new_name="$(echo "$name" | cut -d '/' -f 3 | cut -d '_' -f 1,2,3,4,5 )"; 
+## Prin to console the name of the sample being processed:
 echo "$new_name";
 ./src/STAR/bin/Linux_x86_64_static/STAR  \
 --genomeDir ./star_indexes/ \
 --runThreadN 10 \
+--readFilesCommand zcat \
 --readFilesIn "$name" ./input/"$new_name"_2.*fastq \
 --outFileNamePrefix results/"$new_name";
 ## Convert sam to bam and sort by read name:
